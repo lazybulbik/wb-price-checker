df1de721c29ca18df4d1052214a67cf4718a7aa3
Bulbik <russi0947@gmail.com>
2024-08-07 12:39:38 +0400

    fix token

--- app-sender.py
+++ app-sender.py
@@ -2,7 +2,7 @@ from telebot import TeleBot, types
 
 bot = TeleBot('5694054533:AAGVrj58icZPu0L0_3XEVwa3pP1tLO1TGkU')
 
-btn = types.InlineKeyboardButton(text='test', web_app=types.WebAppInfo(url='https://7709-87-117-189-163.ngrok-free.app'))
+btn = types.InlineKeyboardButton(text='test', web_app=types.WebAppInfo(url='https://3385-94-25-190-56.ngrok-free.app'))
 kb = types.InlineKeyboardMarkup().add(btn)
 
-bot.send_message(chat_id=5061120370, text='test', reply_markup=kb) # 5061120370 
+bot.send_message(chat_id=5981743985, text='test', reply_markup=kb) # 5061120370 
